55d5b288266d189aee49bce3009883ef7dbfcee5
JonathanAlves <jonathanmatheusalves@gmail.com>
2025-09-22 22:04:47 -0300

    Testar Factory Animal

--- Hangman_Game/Program.cs
+++ Hangman_Game/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+CreateWordCreator createAnimal = new AnimalsFactory();
+
+IWordFactory animal = createAnimal.AnimaisCategory();
+
+animal.getCategories();
