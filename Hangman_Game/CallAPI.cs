05d301947714f1b64679e5a85aa5f6e9f32dffe6
Lucas <lgs.08lucas@gmail.com>
2025-09-22 21:14:07 -0300

    Criado singleton para chamadas da api.

---
+++ Hangman_Game/CallAPI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Hangman_Game
+{
+    internal class CallAPI
+    {
+        private static CallAPI _callApi = null;
+
+        private String _uri = "https://random-words-api.kushcreates.com/api?words=1";
+
+        private CallAPI() { }
+
+        public static CallAPI GetInstance()
+        {
+            return _callApi ??= new CallAPI();
+        }
+
+        public async Task<Word> GetWord(String theme, String language)
+        {
+            String finalUri = $"{_uri}&language={language}&category={theme}";
+            Console.WriteLine(finalUri);
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(finalUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    try
+                    {
+                        var jsonString = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(jsonString);
+                        var words = System.Text.Json.JsonSerializer.Deserialize<List<Word>>(jsonString);
+                        return words[0];
+                    }catch(Exception ex)
+                    {
+                        throw new Exception("Failed to parse word from API response", ex);
+                    }
+                }
+                else
+                {
+                    throw new Exception("Failed to fetch word from API");
+                }
+            }
+        }
+    }
+}
--- Hangman_Game/Word.cs
+++ Hangman_Game/Word.cs
@@ -3,6 +3,6 @@ namespace Hangman_Game;
 public class Word
 {
     protected String word { get; set; }
-    protected String Category {get; set;}
-    protected String Language {get; set;}
+    protected String category {get; set;}
+    protected String language {get; set;}
 }
