0d8a8d3aad561c1b8f6c8825684062dc5c8148b8
Matt <realmofthemadzueira@gmail.com>
2025-09-22 22:09:35 -0300

    feat:adicionar UIFacade.cs

---
+++ Hangman_Game/UIFacade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIFacade
+{
+    internal class UIFacade
+    {
+        public void startGame()
+        {
+            Console.WriteLine("Game Started");
+        }
+        public void playTurn()
+        {
+            Console.WriteLine("Turn Played");
+        }
+        public void showStatus()
+        {
+            Console.WriteLine("Status Shown");
+        }
+        public void restart()
+        {
+            Console.WriteLine("Game Restarted");
+        }
+    }
+}
